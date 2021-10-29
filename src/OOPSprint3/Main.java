84f0d7b4a635b6fbf00c6f5c82ad783b6d0ec73b
”Filip <Filip.edfors@hotmail.com>
2021-10-29 10:00:00 +0200

    Creating Main class

---
+++ src/OOPSprint3/Main.java
@@ -0,0 +1,8 @@
+package OOPSprint3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/OOPSprint3/Panel.java
+++ src/OOPSprint3/Panel.java
@@ -28,6 +28,5 @@ public class Panel extends JFrame implements ActionListener {
 
         buttonNewGame.addActionListener(this);
 
-
     }
 }
