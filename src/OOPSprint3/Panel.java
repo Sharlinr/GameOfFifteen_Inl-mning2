f3552042bd1785a995b00adaae7fb2440b635763
”Filip <Filip.edfors@hotmail.com>
2021-10-29 10:37:49 +0200

    Adding for loops in Panel class

--- src/OOPSprint3/Panel.java
+++ src/OOPSprint3/Panel.java
@@ -4,8 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Panel extends JFrame implements ActionListener {
+
+    ArrayList<ButtonsLogic> buttonList = new ArrayList<>();
+    ArrayList<Integer> randomize = new ArrayList<>();
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -27,6 +32,25 @@ public class Panel extends JFrame implements ActionListener {
         p.setLayout(new GridLayout(4, 4));
 
         buttonNewGame.addActionListener(this);
+
+        for (int i = 0; i < 16; ++i) {
+            randomize.add(i);
+        }
+        Collections.shuffle(randomize);
+        for (int i = 0; i < 16; ++i) {
+            String buttonName = "" + randomize.get(i);
+            if (buttonName.equals("0")) {
+                buttonName = "";
+            }
+            buttonList.add(new ButtonsLogic(buttonName, i, randomize.get(i), buttonList, this));
+        }
+
+        for (JButton buttons : buttonList) {
+            p.add(buttons);
+        }
+        pack();
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
     }
 }
