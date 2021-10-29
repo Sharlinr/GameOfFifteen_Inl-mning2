a4b2b5253a1fccc0127f1ba218b8a7df9d5e7503
Sharlin <sharlin.rahman91rgmail.com>
2021-10-29 10:30:38 +0200

    Adding Override methods, if-statements, game logic.

--- src/OOPSprint3/ButtonsLogic.java
+++ src/OOPSprint3/ButtonsLogic.java
@@ -44,6 +44,33 @@ public class ButtonsLogic extends JButton implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (finalPlacement < 12) {           // first, second, third row, looking down.
+            ButtonsLogic buttonBelow = buttons.get(finalPlacement + 4);
+            if (buttonBelow.myDestination == 0) {
+                swap(buttonBelow);
+            }
+        } // Check down
+        if (finalPlacement > 3) {           // Fourth, third, second row, looking up.
+            ButtonsLogic buttonAbove = buttons.get(finalPlacement - 4);
+            if (buttonAbove.myDestination == 0) {
+                swap(buttonAbove);
+            }
+        } // Check up
 
+        if (finalPlacement % 4 != 0) {      // Going left.
+            ButtonsLogic buttonLeft = buttons.get(finalPlacement - 1);
+            if (buttonLeft.myDestination == 0) {
+                swap(buttonLeft);
+            }
+        } // Check left
+        if (finalPlacement % 4 != 3) {      // Going right.
+            ButtonsLogic buttonRight = buttons.get(finalPlacement + 1);
+            if (buttonRight.myDestination == 0) {
+                swap(buttonRight);
+            }
+        } // Check right
     }
+
+
+
 }
